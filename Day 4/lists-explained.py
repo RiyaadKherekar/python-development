20fd2185b880b7fa3579de5bb889f01aa4f7e005
Riyaad Kherekar <rkherekar@gmail.com>
2023-08-29 01:07:09 +0200

    Adding day 4

---
+++ Day 4/lists-explained.py
@@ -0,0 +1,18 @@
+# Lists is basically a data structure
+# You might want to store data in connection to each other as a list
+# This allows you to have order in your data or virtual queue 
+# Example: fruits = [item1, item2] -- in Python, lists always start with a opening square bracket and closes with a closing square bracket
+# fruits = ["Cherry", "Apple"]
+
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+
+# print(states_of_america[1])
+# print(states_of_america[0])
+
+# To alter my list
+# states_of_america[1] = "pencilvania"
+
+# To add item to list
+# states_of_america.append("South Africa")
+# print(states_of_america)
+
