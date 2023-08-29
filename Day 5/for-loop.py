0b8d2afc66def2f631c4e601658c8b3f3f415461
Riyaad Kherekar <rkherekar@gmail.com>
2023-08-29 23:56:05 +0200

    Adding Day 5

---
+++ Day 5/for-loop.py
@@ -0,0 +1,12 @@
+# Example
+# for item in list_of_items
+
+#For Loop with Lists
+# fruits = ["Apple", "Peach", "Pear"]
+# for fruit in fruits:
+#     print(fruit)
+#     print(fruit + " Pie")
+# print(fruits)
+
+for number in range(1, 11, 3):
+    print(number)
