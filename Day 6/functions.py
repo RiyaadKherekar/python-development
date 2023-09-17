307047d5f5ffdfb5ad0249a61a4230f42e0e930c
Riyaad Kherekar <rkherekar@gmail.com>
2023-09-17 22:40:30 +0200

    update

---
+++ Day 6/functions.py
@@ -0,0 +1,26 @@
+# How do we know what a function is?
+# It is followed by a set of parathesis ()
+
+print("Hello")
+num_char = len("Hello")
+print(num_char)
+
+# Create our own function
+
+def my_function():
+    print("Hello")
+    print("Bye")
+    
+# Trigger/Call function:
+my_function()
+
+
+# Defining a function
+# def my_function():
+    #do this
+    #then do this
+    # finally do this
+    
+# Calling functions
+# Specifying the name of the function
+
