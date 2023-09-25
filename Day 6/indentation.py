67ea13a24abcbf5b48187e3923069a349c32702d
Riyaad Kherekar <rkherekar@gmail.com>
2023-09-25 17:40:31 +0200

    Adding files

---
+++ Day 6/indentation.py
@@ -0,0 +1,16 @@
+## Functions are like a folder structure, with its indentation. 
+## Indents are 4 spaces
+
+def my_function():
+    print("Hello")
+print("Hello") # This is not inside the function due to not having an indentation. 
+
+def my_function():
+    if sky == "clear": #x 4 spaces
+        print("blue") #x 8 spaces
+    elif sky == "cloudy":
+        print("grey")
+    print("Hello")
+print("Hello")
+
+# Spaces vs Tabs
---
+++ Day 6/while_loop.py
@@ -0,0 +1,21 @@
+# For Loops:
+# for item in list_of_items:
+#     do something to each item
+    
+# for number in range(a, b):
+#     print(number)
+
+# While Loops:
+# while something_is_true:
+#     do something repeatedly 
+    
+# Only when something becoems false, the while loop will stop.
+
+# Example of While Loop using Reeborgs World:
+    
+number_of_hurdles = 6
+
+while number_of_hurdles > 0:
+    jump()
+    number_of_hurdles -= 1
+    print(number_of_hurdles)
